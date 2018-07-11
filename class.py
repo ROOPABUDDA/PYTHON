96c9ad59a149610b1c2f20d9a5178634f9131b90
ROOPA <roopabudda@gmail.com>
2018-07-11 15:35:34 +0530

    [OOPS]basic class

---
+++ class.py
@@ -0,0 +1,12 @@
+class MyClass(object):
+	"""docstring for MyClass
+"""
+	def __init__(self, arg):
+		super(MyClass
+		, self).__init__()
+		self.arg = arg
+	a=50
+	def func(self):
+		print('Hello World')
+
+print(MyClass.a)
