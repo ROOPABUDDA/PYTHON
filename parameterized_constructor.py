1fe1a0302946ee73122ae194d887d08278812c6b
ROOPA <roopabudda@gmail.com>
2018-07-11 17:17:24 +0530

    [OOPS]parameterised constructor

--- parameterized_constructor.py
+++ parameterized_constructor.py
@@ -1,9 +1,10 @@
 class Student:
 	"""this is a parameterised constructor"""
-	def __init__(self):
-		print("This is non parameterised constructor")
-	def show(self,name):
-		print("Hello {}".format(name))
+	def __init__(self,name):
+		print("This is parameterised constructor")
+		self.name = name
+	def show(self):
+		print("Hello {}".format(self.name))
 
 student1 = Student()
 student1.show("Roopa")
